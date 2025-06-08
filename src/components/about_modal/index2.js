08e27ab3e6081e9847e9eaf926125a627d1650b7
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-06-08 19:28:25 +0200

    add function to calculate viewport

--- src/components/about_modal/index2.js
+++ src/components/about_modal/index2.js
@@ -48,20 +48,22 @@ export default function AboutModal2({isOpen, onClose}) {
     >
       <div
         ref={modalRef}
-        className="absolute flex flex-col top-1/2 text-xl text-justify left-1/2 z-50 w-[95vw] md:w-[60vw] -translate-x-1/2 -translate-y-1/2 
-                   bg-white/90 backdrop-blur-sm text-black px-12 md:px-24 py-8 md:py-16 gap-2 md:gap-4"
+        className="absolute flex flex-col top-1/2 text-base text-justify left-1/2 z-50 w-[95vw] md:w-[60vw] -translate-x-1/2 -translate-y-1/2 
+                   bg-white/90 backdrop-blur-sm text-black px-3 md:px-24 py-8 md:py-16 gap-2 md:gap-4"
       >
-        <div className="flex flex-col">
-          <p className="flex leading-none">
+        <div className="flex flex-col uppercase">
+          <p className="flex leading-tight">
             In the last few years, Johnny Carretes has been exploring life accompanied by a 35mm camera.
-            His journey with analog photography began innocently, playing with disposable cameras during summer vacations.
           </p>
-          <p className="flex leading-none pt-2">
+          <p className="flex leading-tight pt-3">
+          His journey with analog photography began innocently, playing with disposable cameras during summer vacations.
+          </p>
+          <p className="flex leading-tight pt-3">
             Captivated by the analog feel, the magic and uncertainty inherent in format, his photography attempts to combine beauty, nostalgia and authenticity.
           </p>
         </div>
-        <div className="flex flex-col -space-y-2">
-          <h2 className="uppercase flex">REQUESTS</h2>
+        <div className="flex flex-col -space-y-1 uppercase">
+          <h2 className="flex">REQUESTS</h2>
           <p className="flex">
           johnny.carretes@gmail.com
           </p>
