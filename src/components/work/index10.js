cc6fb12cdb6f3b692cb380d900012afb8b26ca60
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-06-17 23:14:21 +0200

    Back to old mobile gallery titles

--- src/components/work/index10.js
+++ src/components/work/index10.js
@@ -5,6 +5,7 @@ import Project6 from "@/components/work/project6";
 import useIsMobile from "@/hooks/useIsMobile";
 // import ProjectMobile4 from "./ProjectMobile4";
 import ProjectMobile5 from "./ProjectMobile5";
+import ProjectMobile4 from "./ProjectMobile4";
 
 const projects = [
 
@@ -27,12 +28,12 @@ const projects = [
     id: "johnny_color",
     title1: "johnny",
     title2: "color",
-    mobileTitle1: "johnny",
-    mobileTitle2: "color",
-    mobileLayout: [
-      ["mobileTitle1"],
-      ["mobileTitle2"]
-  ],
+  //   mobileTitle1: "johnny",
+  //   mobileTitle2: "color",
+  //   mobileLayout: [
+  //     ["mobileTitle1"],
+  //     ["mobileTitle2"]
+  // ],
     src: "image6.webp"
   },
 
@@ -41,13 +42,13 @@ const projects = [
     title1: "leak",
     title2: "of",
     title3: "dreams",
-    mobileTitle1: "leak",
-    mobileTitle2: "of",
-    mobileTitle3: "dreams",
-    mobileLayout: [
-      ["mobileTitle1", "mobileTitle2"],
-      ["mobileTitle3"]
-  ],
+  //   mobileTitle1: "leak",
+  //   mobileTitle2: "of",
+  //   mobileTitle3: "dreams",
+  //   mobileLayout: [
+  //     ["mobileTitle1", "mobileTitle2"],
+  //     ["mobileTitle3"]
+  // ],
     src: "image14.webp"
   },
 
@@ -55,13 +56,13 @@ const projects = [
     id: "costas_de_sol",
     title1: "costas",
     title2: "de sol",
-    mobileTitle1: "costas",
-    mobileTitle2: "de",
-    mobileTitle3: "sol",
-    mobileLayout: [
-      ["mobileTitle1"],
-      ["mobileTitle2", "mobileTitle3"]
-  ],
+  //   mobileTitle1: "costas",
+  //   mobileTitle2: "de",
+  //   mobileTitle3: "sol",
+  //   mobileLayout: [
+  //     ["mobileTitle1"],
+  //     ["mobileTitle2", "mobileTitle3"]
+  // ],
     src: "image65.webp"
   },
 
@@ -90,7 +91,7 @@ const WorkMenu10 = () => {
     <div className="min-h-screen w-screen overflow-hidden text-black flex flex-col justify-center items-center">
       {projects.map((project) =>
         isMobile ? (
-          <ProjectMobile5
+          <ProjectMobile4
           key={project.id}
           project={project}
           selectedId={selectedId}
